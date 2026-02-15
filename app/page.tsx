8eddb946446e601005a4f6d1b15a9831745a4e18
CORHYN <marzouqhashmi@gmail.com>
2026-02-15 11:29:15 +0300

    feat: centered the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -25,14 +25,14 @@ export default function Home() {
 		<main className="min-h-screen flex flex-col items-center">
 			<div className="flex-1 w-full flex flex-col">
 				<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-					<div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+					<div className="container mx-auto flex justify-between lg:px-10 px-4">
 						<div className="flex gap-5 items-center">
 							<ThemeSwitcher />
 						</div>
+						<Suspense>
+							<AuthButton />
+						</Suspense>
 					</div>
-					<Suspense>
-						<AuthButton />
-					</Suspense>
 				</nav>
 				<main className="flex-1 flex flex-col gap-6 2xl:px-16 px-4 w-full container mx-auto">
 					<div className="flex justify-end pt-10 pb-5">
@@ -42,7 +42,9 @@ export default function Home() {
 						<BookmarkList />
 					</Suspense>
 				</main>
-				<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16"></footer>
+				<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+                    <div>This is a bookmarker app made for a micro-challenge © 2026</div>
+                </footer>
 			</div>
 		</main>
 	);
